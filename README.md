c7a62ea6cfd0227ac63a32319883b8394a889c8e
jawerty <jawerty210@gmail.com>
2012-11-01 16:20:40 -0400

    Change README and footer

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 <h1>NinjaMine</h1>
 NinjaMine (pseudo-name for alpha version) is a website for posting user-generated quotes. The more viewed or well-received a quote is, the more accolades and notoriety the quote's author will receive.
 
+<br>
+Currently tested on Google Chrome (only).
+<br>
 v: 0.0.1 
 url: http://ninjamine.herokuapp.com
--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,10 @@ var mongoose = require( 'mongoose' );
 var user     = mongoose.model( 'user' );
 var error;
 
-exports.profile = function(req, res) {
+exports.profile = function(req, res, next) {
     //var author = quotes[req.params.id].author;
     res.render('profile', { title: 'NinjaMine | Profile page - in production'});
-
-
+    
 };
 exports.profile_post_handler = function(req, res) {
 
